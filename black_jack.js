b61e73da65ce89df341de838a2fc51d6c9c1e1c8
monkyz <gabrieluk@cooltoad.com>
2013-02-28 01:12:21 +0000

    Update black_jack.js
    add getValue()

--- black_jack.js
+++ black_jack.js
@@ -1,12 +1,22 @@
-//start the game
-function Card(n,s) {
-        var number = n;
-        var suit = s;
-        //getters
-        this.getNumber = function(){
-                return number;
-        };
-        this.getSuit = function(){
-                return suit;
-        };
+/start the game
+function Card(n, s) {
+    var number = n;
+    var suit = s;
+    //getters
+    this.getNumber = function () {
+        return number;
+    };
+    this.getSuit = function () {
+        return suit;
+    };
+    this.getValue = function () {
+
+        if (number > 10) {
+            return 10;
+        } else if (number === 1) {
+            return 11;
+        } else {
+            return number;
+        }
+    };
 }
